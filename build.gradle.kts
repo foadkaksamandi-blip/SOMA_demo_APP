139ca9f02e72683d6c5bf41b24f6bcad42467cd4
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-21 16:28:55 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,4 @@ plugins {
     id("com.android.library") version "8.5.2" apply false
     kotlin("android") version "1.9.24" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// توجه: عمداً هیچ repositories اضافه نکردیم تا با تنظیمات هم‌خوان باشد.
