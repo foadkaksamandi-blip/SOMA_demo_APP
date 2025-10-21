2b8c547afbfd800a2b7b0614a6d7a6a4337345ca
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-21 15:56:59 +0330

    Create DateUtils.kt

---
+++ merchant-app/src/main/java/shared/utils/DateUtils.kt
@@ -0,0 +1,10 @@
+package shared.utils
+
+import java.util.Calendar
+import java.util.Locale
+
+object DateUtils {
+    fun nowJalaliDate(): String = shared.utils.DateUtils.nowJalaliDate()
+    fun nowJalaliDateTime(): String = shared.utils.DateUtils.nowJalaliDateTime()
+    fun generateTxId(): String = shared.utils.DateUtils.generateTxId()
+}
