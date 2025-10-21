1b27e4641b1d71ea9bb2d27face38aac582d48eb
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-21 18:52:30 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,26 @@
+pluginManagement {
+    repositories {
+        // برای resolve کردن پلاگین‌های اندروید از Google
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        // نسخه‌ها مطابق پروژه
+        id("com.android.application") version "8.5.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "SOMA_demo_APP"
 
-include(":consumer-app")
-include(":merchant-app")
+// حتما اسم دایرکتوری‌ها همین است:
+include(":consumer-app", ":merchant-app")
