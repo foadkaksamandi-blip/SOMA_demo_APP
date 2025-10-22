afcd3103541f882578a89f2c7cf80bbcb2667480
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-22 16:45:04 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,15 @@
-// ===== Settings for SOMA_demo_APP =====
-
 pluginManagement {
     repositories {
-        // برای پلاگین‌های Gradle (Android, Kotlin, …)
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
-    // اجازه ندیم هر ماژول خودش ریپوی جدا تعریف کنه
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // برای دیپندنسی‌های پروژه
         google()
         mavenCentral()
-        // اگر بعداً به JitPack نیاز شد، این را فعال کن:
-        // maven(url = "https://jitpack.io")
     }
 }
 
