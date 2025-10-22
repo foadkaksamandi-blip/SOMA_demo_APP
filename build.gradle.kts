4ce5258b7d48b35fcc5248ed053f04ea55a4264c
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-22 14:37:29 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,10 @@
+// فایل ریشه پروژه SOMA_demo_APP
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-android {
-    namespace = "com.soma.consumer"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.soma.consumer"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildFeatures {
-        viewBinding = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("com.google.android.material:material:1.12.0")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
