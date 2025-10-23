cf398a3f4f7e5f5619a28ac8b17985b20b41f1d2
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-23 14:40:12 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,8 @@
-// Top-level build file for SOMA_demo_APP
-
 plugins {
     id("com.android.application") version "8.7.0" apply false
     kotlin("android") version "1.9.24" apply false
 }
 
-// هیچ repositoriesاینجا نگذار—مخازن فقط در settings.gradle.kts تعریف شوند.
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-```0
