9f890b53b3fae915618c105bf65b132ec7737e63
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-23 15:19:11 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("com.android.application") version "8.7.0" apply false
-    kotlin("android") version "1.9.24" apply false
+    id("com.android.application") version "8.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+```0
