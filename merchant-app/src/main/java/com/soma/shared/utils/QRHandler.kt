69d39855b048938a8fbe78f36c930c6ae93e8673
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-24 15:00:29 +0330

    Update QRHandler.kt

--- merchant-app/src/main/java/com/soma/shared/utils/QRHandler.kt
+++ merchant-app/src/main/java/com/soma/shared/utils/QRHandler.kt
@@ -1,34 +1,20 @@
-package com.soma.shared.utils
+package com.soma.merchant.utils
 
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.widget.ImageView
 import com.google.zxing.BarcodeFormat
-import com.google.zxing.BinaryBitmap
-import com.google.zxing.MultiFormatReader
-import com.google.zxing.NotFoundException
-import com.google.zxing.common.BitMatrix
-import com.google.zxing.common.HybridBinarizer
 import com.google.zxing.qrcode.QRCodeWriter
-import com.google.zxing.RGBLuminanceSource
 
 object QRHandler {
-    fun generate(content: String, size: Int = 600): Bitmap {
-        val matrix: BitMatrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, size, size)
+    fun renderTo(target: ImageView, text: String, size: Int = 600) {
+        val bits = QRCodeWriter().encode(text, BarcodeFormat.QR_CODE, size, size)
         val bmp = Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565)
         for (x in 0 until size) {
             for (y in 0 until size) {
-                bmp.setPixel(x, y, if (matrix[x, y]) 0xFF000000.toInt() else 0xFFFFFFFF.toInt())
+                bmp.setPixel(x, y, if (bits[x, y]) Color.BLACK else Color.WHITE)
             }
         }
-        return bmp
-    }
-
-    fun decodeFromBitmap(bitmap: Bitmap): String? {
-        val w = bitmap.width
-        val h = bitmap.height
-        val pixels = IntArray(w * h)
-        bitmap.getPixels(pixels, 0, w, 0, 0, w, h)
-        val source = RGBLuminanceSource(w, h, pixels)
-        val binBmp = BinaryBitmap(HybridBinarizer(source))
-        return try { MultiFormatReader().decode(binBmp).text } catch (_: NotFoundException) { null }
+        target.setImageBitmap(bmp)
     }
 }
