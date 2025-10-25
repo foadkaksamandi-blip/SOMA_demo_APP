ed4ecb27366292c5477b9d73f5026d127126f6be
foadkaksamandi-blip <foadkaksamandi@gmail.com>
2025-10-25 20:49:21 +0330

    Update MainActivity.kt

--- merchant-app/src/main/java/com/soma/merchant/MainActivity.kt
+++ merchant-app/src/main/java/com/soma/merchant/MainActivity.kt
@@ -1,15 +1,10 @@
-package com.soma.merchant
+package com.example.merchant
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.soma.merchant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.textView.text = "Merchant App is running"
     }
 }
